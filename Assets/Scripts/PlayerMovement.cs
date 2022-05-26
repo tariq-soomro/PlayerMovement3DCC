b69f856ea6bd5e563a2f428ea9b9af9f1dd70c12
Tariq Soomro <52823477+tariq-soomro@users.noreply.github.com>
2022-05-26 14:20:05 +0500

    Added PlayerMovement and MouseLook scripts

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public CharacterController controller;
+
+    public Transform GroundCheck;
+    public float groundDistance = 0.4f;
+    public LayerMask groundMask;
+
+    public float speed = 12f;
+    public float gravity = -9.81f;
+    public float jumpHeight = 3f;
+
+    Vector3 velocity;
+    bool isGrounded = false;
+
+	void Update()
+    {
+        isGrounded = Physics.CheckSphere(GroundCheck.position, groundDistance, groundMask);
+
+        if (isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+
+        /*
+        if (!isGrounded && velocity.y < 0)
+        {
+            velocity.y = -5f;
+        }*/
+
+        float x = Input.GetAxisRaw("Horizontal");
+        float z = Input.GetAxisRaw("Vertical");
+
+        Vector3 move = transform.right * x + transform.forward * z;
+
+        controller.Move(move.normalized * speed * Time.deltaTime);
+
+        if (Input.GetButton("Jump") && isGrounded)
+        {
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+        }
+
+        velocity.y += gravity * Time.deltaTime;
+
+        controller.Move(velocity * Time.deltaTime);
+    }
+}
